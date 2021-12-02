b3adadf2db5f01d9e4ccdf118a6c7097a65bba12
Alexandria Burnett <thekaylisia@gmail.com>
2021-12-01 20:39:54 -0500

    View job details initial

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ window.addEventListener("load", (e)=>{
             log=false;
         }
     });
-
+    //loadvjPage();
 });
 
 function ajaxRequest(query){
@@ -63,4 +63,75 @@ function ajaxRequest(query){
     httpRequest.open('Get', url, true);
     httpRequest.send();
     url = "http://localhost/info2180-finalproject/backend.php";
+}
+
+//View job details functions
+//var issueid=1;
+var url2 = "http://localhost/info2180-finalproject/viewjob.php";
+
+
+function initvjpage(mcbutton,mipbutton){ // adds event listeners to the viewjobdetails page
+    //unable to acess elements made in php
+    
+    mcbutton.addEventListener("click",function(event){
+        event.preventDefault();
+        console.log("MC");
+        changeStatus("Closed",issueid);
+    });//mc event listener
+    mipbutton.addEventListener("click",function(event){
+        event.preventDefault();
+        console.log("MIP");
+        changeStatus("In-Progress",issueid);
+    });//mip event listener
+}
+function loadvjPage(){ //should have issueid as a parameter
+    //load page will be the event listener for whatever options are clicked.
+    // get id from somewhere
+    vjajaxRequest(issueid,"issid");
+
+}
+function changeStatus(stat,issueid){
+     httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE){
+            if (httpRequest.status === 200) {              
+                vjajaxRequest(issueid,"issid");  
+                //location.reload();              
+            } else {
+                alert('There was a problem with the request.');
+            }
+        } 
+    }
+    staturl="http://localhost/info2180-finalproject/viewjob.php";
+
+    staturl= staturl+ "?issid="+issueid+"&status="+stat;
+
+    httpRequest.open('Get', staturl, true);
+    httpRequest.send();
+    
+}
+
+function vjajaxRequest(query,param){
+    httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE){
+            if (httpRequest.status === 200) {
+                var response = httpRequest.responseText;
+                var resultdiv= document.getElementById("main");
+                resultdiv.innerHTML=response;
+                var mcbutton=document.getElementById("vjclosedbutton");
+                var mipbutton=document.getElementById('vjipbutton');
+                
+                initvjpage(mcbutton,mipbutton);
+            } else {
+                alert('There was a problem with the request.');
+            }
+        } 
+    }
+
+    url2= url2 + "?"+param+"=" + query;
+
+    //console.log(url2);
+
+    httpRequest.open('Get', url2, true);
+    httpRequest.send();
+    url2 = "http://localhost/info2180-finalproject/viewjob.php";
 }
---
+++ viewjob.php
@@ -0,0 +1,83 @@
+<?php session_start(); 
+$host = 'localhost';
+$username = 'project2';
+$password = 'password123';
+$dbname = 'bugme';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$issid= filter_input(INPUT_GET,"issid",FILTER_SANITIZE_STRING);
+loadJob($issid, $conn);
+
+changeStatus($conn);
+?>
+<?php 
+function loadJob($issid, $conn){
+
+$stmt = $conn->prepare("SELECT * FROM issues WHERE id=? LIMIT 1");
+$stmt->execute([$issid]);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$cdate = date_create($result['created']);
+$udate = date_create($result['updated']);
+
+if (isset($_GET['issid']))
+	{
+
+	//finding the person assigned to
+	$stm2 = $conn->prepare("SELECT firstname,lastname FROM users WHERE id=? LIMIT 1");
+	$stm2->execute([$result['assigned_to']]);
+	$assigned = $stm2->fetch(PDO::FETCH_ASSOC);
+
+	//finding the person who created
+	$stm3 = $conn->prepare("SELECT firstname,lastname FROM users WHERE id=? LIMIT 1");
+	$stm3->execute([$result['created_by']]);
+	$createdby = $stm3->fetch(PDO::FETCH_ASSOC);
+?>
+	<div id="viewjob">
+		<div class="vjheadings">
+			<h1><?= $result['title'];?></h1> 
+			<h4>Issue #<?= $result['id'];?></h4>
+		</div>
+		<div class="vjmain">
+			<p> <?= $result['description'];?></p>
+			<ul class="cre-upt">
+				<li>Issue created on <?= date_format($cdate,'F j, Y');?> at <?= date_format($cdate,'g:i A'); ?> by <?=$createdby['firstname']?> <?=$createdby['lastname']; ?></li>
+				<li>Last updated on <?= date_format($udate,'F j, Y');?> at <?= date_format($udate,'g:i A'); ?></li>
+			</ul>
+		</div>
+		<div class="vjaddinfo">
+			<p class="bold">Assigned to</p>
+			<p> <?=$assigned['firstname']?> <?=$assigned['lastname'];?></p>
+			<p class="bold">Type</p>
+			<p> <?=$result['type'];?></p>
+			<p class="bold">Priority</p>
+			<p> <?=$result['priority'];?></p>
+			<p class="bold">Status</p>
+			<p><?= $result['status'];?></p>
+
+			
+		</div>
+		<div class="vjbuttonarea">
+			<button id="vjclosedbutton" class="vjbutton">Mark as Closed</button>
+			<button id="vjipbutton" class="vjbutton">Mark in Progress</button>
+		</div>
+
+	</div>
+
+<?php 
+	}
+}
+?>
+
+<?php
+	function changeStatus($conn){ 
+	$status= filter_input(INPUT_GET,"status",FILTER_SANITIZE_STRING);
+	$issid= filter_input(INPUT_GET,"issid",FILTER_SANITIZE_STRING);
+	if(isset($_GET['status']) && isset($_GET['issid'])){
+		$stmt = $conn->prepare("UPDATE issues SET status=? WHERE id=?");
+		$stmt->execute([$status,$issid]);
+
+	}
+}
+
+?>
