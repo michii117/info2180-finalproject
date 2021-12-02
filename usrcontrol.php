b1be4e013a33a52ba2632530a75da1125966e8e5
Riverine-jpg <nateb2001@gmail.com>
2021-12-02 17:28:18 -0500

    random } missing

--- usrcontrol.php
+++ usrcontrol.php
@@ -6,7 +6,7 @@
         $lname = htmlentities($_POST['Lname'],ENT_QUOTES, 'UTF-8');
         $email = htmlentities($_POST['email'],ENT_QUOTES, 'UTF-8');
         $pass = htmlentities($_POST['password'],ENT_QUOTES, 'UTF-8');
-    
+    }
     $sql = "INSERT INTO `users` (`ID`, `firstname`, `lastname`, `password`, `email`, `date_joined`) VALUES (NULL, '$fname', '$lname', MD5('$pass'), '$email',  NOW())";
     
     if (mysqli_query($connection, $sql)) {
